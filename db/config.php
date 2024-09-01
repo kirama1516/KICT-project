f6787d70d9598379b342307bd229b573688c2e34
kirama1516 <135828088+kirama1516@users.noreply.github.com>
2024-09-01 12:32:43 +0100

    Update config.php

--- db/config.php
+++ db/config.php
@@ -6,7 +6,7 @@ require_once 'vendor/autoload.php';
 
 // init config
 $clientID = '971308071057-qu8a0np2rlocrdl1l5k80pep4gilifte.apps.googleusercontent.com';
-$clientSecret = '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';
+$clientSecret = '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';
 $redirectURL = 'http://localhost/kict-project/google-dashboard.php';
 
 // Set up the Google client
